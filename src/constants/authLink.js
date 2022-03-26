4994b26a3db9caf762bd9b5ca137615a291c985c
kage1011 <khaivv123@gmail.com>
2022-03-26 16:03:26 +0700

    route & classes admin +

--- src/constants/authLink.js
+++
@@ -1,6 +0,0 @@
-const authLink = {
-    googleAuth: "http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:8080/oauth2/redirect",
-    facebookAuth: ''
-}
-
-export default authLink;
